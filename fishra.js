4d0b4c6375909c0c6d47bf2fcb2b984247401d7c
cecilehueb <cecilehubner@gmail.com>
2022-11-16 23:15:09 +0100

    videopage, sidebar, links

--- fishra.js
+++ fishra.js
@@ -9,12 +9,38 @@ let images = [
 	"img/randomimg/photo04.jpg",
 	"img/randomimg/photo05.PNG",
 	"img/randomimg/photo06.jpg",
+	"img/randomimg/photo07.jpg",
+	"img/randomimg/photo08.jpg",
+	"img/randomimg/photo09.jpg",
+	"img/randomimg/photo10.jpg",
+	"img/randomimg/photo11.jpg",
+	"img/randomimg/photo12.jpg",
+	"img/randomimg/photo13.jpg",
+	"img/randomimg/photo14.jpg",
+	"img/randomimg/photo15.jpg",
+	"img/randomimg/photo16.jpg",
+	"img/randomimg/photo17.jpg",
+	"img/randomimg/photo18.jpg",
+	"img/randomimg/photo19.jpg",
+	"img/randomimg/photo20.jpg",
+	"img/randomimg/photo21.jpg",
+	"img/randomimg/photo22.jpeg",
+	"img/randomimg/photo23.jpg",
+	"img/randomimg/photo24.jpg",
+	"img/randomimg/photo25.jpg",
+	"img/randomimg/photo26.jpg",
+	"img/randomimg/photo27.jpg",
+	"img/randomimg/photo28.jpeg",
+	"img/randomimg/photo29.jpg",
+	"img/randomimg/photo30.jpeg",
+	"img/randomimg/photo31.JPG",
+
 ]
 
 	// defines a function and calls every 1 second
 var loop;
 function imageLoop() {
-	loop = setInterval(getRandomImage, 1000);
+	loop = setInterval(getRandomImage, 700);
 }
 
 imageLoop();
